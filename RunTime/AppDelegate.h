8927b8b7bd74afd21cf39b0dfe6a870856cbb035
HMC <jarthur@HMCs-MacBook-Pro.local>
2014-02-23 19:58:54 -0800

    Initial Commit

---
+++ RunTime/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RunTime
+//
+//  Created by HMC on 2/23/14.
+//  Copyright (c) 2014 HMC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RunTimeTests/RunTimeTests.m
@@ -0,0 +1,34 @@
+//
+//  RunTimeTests.m
+//  RunTimeTests
+//
+//  Created by HMC on 2/23/14.
+//  Copyright (c) 2014 HMC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RunTimeTests : XCTestCase
+
+@end
+
+@implementation RunTimeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
