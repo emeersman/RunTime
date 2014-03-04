2584d161cce58945017620d6dabe63b215b0c7ea
Emma <emma.meersman@gmail.com>
2014-03-03 21:05:53 -0800

    We added stuff

--- QuickTimerViewController.h
+++ QuickTimerViewController.h
@@ -8,13 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
-@interface QuickTimerViewController : UIViewController
+@interface QuickTimerViewController : UIViewController <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UIButton *startTimer;
 @property (weak, nonatomic) IBOutlet UIButton *resetTimer;
-
+@property (weak, nonatomic) IBOutlet UITextField *timerHr;
+@property (weak, nonatomic) IBOutlet UITextField *timerMin;
 @property (weak, nonatomic) IBOutlet UITextField *timerSec;
 @property (strong, nonatomic) NSTimer *quickTimer;
+@property (weak, nonatomic) IBOutlet UIImageView *titleImage;
+
+//- (void)resetMin;
+//- (void)resetSec;
 
 extern NSInteger secValue;
+extern NSInteger minValue;
+extern NSInteger hrValue;
 extern BOOL started;
 @end
--- QuickTimerViewController.m
+++ QuickTimerViewController.m
@@ -14,8 +14,12 @@
 
 @implementation QuickTimerViewController
 
-int secValue = 30;
-int secCountDown = 30;
+int secValue = 0;
+int secCountDown = 0;
+int minValue = 0;
+int minCountDown = 0;
+int hrValue = 0;
+int hrCountDown = 0;
 BOOL started = NO;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -28,17 +32,34 @@ BOOL started = NO;
     return self;
 }
 
-
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.timerSec.text = [NSString stringWithFormat:@"%d", secValue];
-	
+    self.timerMin.delegate = self;
+    self.timerSec.delegate = self;
+    self.timerHr.delegate = self;
+    //_titleImage.image = [UIImage imageNamed:@"heman.jpg"];
 }
 
--(void)updateTimer:(NSTimer *)timer {
-    self.timerSec.text = [NSString stringWithFormat:@"%d", --secCountDown];
+- (IBAction)secValueChanged:(id)sender {
+    secValue = [_timerSec.text intValue];
+    secCountDown = [_timerSec.text intValue];
+}
+
+- (IBAction)minValueChanged:(id)sender {
+    minValue = [_timerMin.text intValue];
+    minCountDown = [_timerMin.text intValue];
+}
+
+- (IBAction)hrValueChanged:(id)sender {
+    hrValue = [_timerHr.text intValue];
+    hrCountDown = [_timerHr.text intValue];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *) timerField {
+    [self.view endEditing:YES];
+    [timerField resignFirstResponder];
+    return YES;
 }
 
 - (IBAction)beginTimer:(id)sender {
@@ -51,7 +72,7 @@ BOOL started = NO;
         
         NSRunLoop *runner = [NSRunLoop currentRunLoop];
         [runner addTimer: self.quickTimer forMode: NSDefaultRunLoopMode];
-        [_startTimer setTitle:@"Pause" forState:UIControlStateNormal];
+        [sender setTitle:@"Pause" forState:UIControlStateNormal];
         started = YES;
     }
     else {
@@ -62,8 +83,96 @@ BOOL started = NO;
             _quickTimer = nil;
         }
     }
-
+    
 }
+/*
+-(void)resetSec {
+    if(secCountDown > 0)
+    {
+        self.timerSec.text = [NSString stringWithFormat:@"%d", --secCountDown];
+    }
+}
+
+-(void)resetMin {
+    if(minCountDown > 0) {
+        if(secCountDown == 0) {
+            self.timerMin.text = [NSString stringWithFormat:@"%d", --minCountDown];
+            self.timerSec.text = [NSString stringWithFormat:@"%d", 59];
+            secCountDown = 59;
+        }
+        else
+        {
+            self.timerSec.text = [NSString stringWithFormat:@"%d", --secCountDown];
+        }
+    }
+    else{
+        self.timerSec.text = [NSString stringWithFormat:@"%d", --secCountDown];
+    }
+}*/
+
+-(void)updateTimer:(NSTimer *)timer {
+    if(hrCountDown > 0)
+    {
+        if(minCountDown == 0 && secCountDown == 0)
+        {
+            self.timerHr.text = [NSString stringWithFormat:@"%d", --hrCountDown];
+            self.timerMin.text = [NSString stringWithFormat:@"%d", 59];
+            self.timerSec.text = [NSString stringWithFormat:@"%d", 59];
+            minCountDown = 59;
+            secCountDown = 59;
+        }
+        else {
+            if(minCountDown > 0) {
+                if(secCountDown == 0) {
+                    self.timerMin.text = [NSString stringWithFormat:@"%d", --minCountDown];
+                    self.timerSec.text = [NSString stringWithFormat:@"%d", 59];
+                    secCountDown = 59;
+                }
+                else
+                {
+                    self.timerSec.text = [NSString stringWithFormat:@"%d", --secCountDown];
+                }
+            }
+            else{
+                self.timerSec.text = [NSString stringWithFormat:@"%d", --secCountDown];
+            }
+        }
+    }
+    else {
+        if(minCountDown > 0) {
+            if(secCountDown == 0) {
+                self.timerMin.text = [NSString stringWithFormat:@"%d", --minCountDown];
+                self.timerSec.text = [NSString stringWithFormat:@"%d", 59];
+                secCountDown = 59;
+            }
+            else
+            {
+                self.timerSec.text = [NSString stringWithFormat:@"%d", --secCountDown];
+            }
+        }
+        else{
+            if(secCountDown > 0)
+            {
+                self.timerSec.text = [NSString stringWithFormat:@"%d", --secCountDown];
+                _titleImage.image = [UIImage imageNamed:@"heman.jpg"];
+            }
+            else
+            {
+                _titleImage.image = [UIImage imageNamed:@"skeletor.jpg"];
+            }
+        }
+    }
+    if(hrCountDown < 10)
+        _timerHr.text = [NSString stringWithFormat:@"0%d", hrCountDown];
+    if(minCountDown < 10)
+        _timerMin.text = [NSString stringWithFormat:@"0%d", minCountDown];
+    if(secCountDown < 10)
+        _timerSec.text = [NSString stringWithFormat:@"0%d", secCountDown];
+}
+
+
+
+
 
 - (IBAction)resetTimer:(id)sender {
     if(_quickTimer){
@@ -72,6 +181,10 @@ BOOL started = NO;
     }
     _timerSec.text = [NSString stringWithFormat:@"%d", secValue];
     secCountDown = secValue;
+    _timerMin.text = [NSString stringWithFormat:@"%d", minValue];
+    minCountDown = minValue;
+    _timerHr.text = [NSString stringWithFormat:@"%d", hrValue];
+    hrCountDown = hrValue;
 }
 
 - (void)didReceiveMemoryWarning
